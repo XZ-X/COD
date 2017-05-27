748c64f2120cf9e26617b144e1c172a6e6d76060
XXZ <161250170@mail.smail.nju.edu.cn>
2017-05-27 16:17:42 +0800

    build the project

---
+++ test/ALUTest.java
@@ -0,0 +1,95 @@
+import static org.junit.Assert.*;
+
+/**
+ * Created by xuxiangzhe on 2017/5/27.
+ */
+public class ALUTest {
+    @org.junit.Test
+    public void integerRepresentation() throws Exception {
+    }
+
+    @org.junit.Test
+    public void floatRepresentation() throws Exception {
+    }
+
+    @org.junit.Test
+    public void ieee754() throws Exception {
+    }
+
+    @org.junit.Test
+    public void integerTrueValue() throws Exception {
+    }
+
+    @org.junit.Test
+    public void floatTrueValue() throws Exception {
+    }
+
+    @org.junit.Test
+    public void negation() throws Exception {
+    }
+
+    @org.junit.Test
+    public void leftShift() throws Exception {
+    }
+
+    @org.junit.Test
+    public void logRightShift() throws Exception {
+    }
+
+    @org.junit.Test
+    public void ariRightShift() throws Exception {
+    }
+
+    @org.junit.Test
+    public void fullAdder() throws Exception {
+    }
+
+    @org.junit.Test
+    public void claAdder() throws Exception {
+    }
+
+    @org.junit.Test
+    public void oneAdder() throws Exception {
+    }
+
+    @org.junit.Test
+    public void adder() throws Exception {
+    }
+
+    @org.junit.Test
+    public void integerAddition() throws Exception {
+    }
+
+    @org.junit.Test
+    public void integerSubtraction() throws Exception {
+    }
+
+    @org.junit.Test
+    public void integerMultiplication() throws Exception {
+    }
+
+    @org.junit.Test
+    public void integerDivision() throws Exception {
+    }
+
+    @org.junit.Test
+    public void signedAddition() throws Exception {
+    }
+
+    @org.junit.Test
+    public void floatAddition() throws Exception {
+    }
+
+    @org.junit.Test
+    public void floatSubtraction() throws Exception {
+    }
+
+    @org.junit.Test
+    public void floatMultiplication() throws Exception {
+    }
+
+    @org.junit.Test
+    public void floatDivision() throws Exception {
+    }
+
+}
